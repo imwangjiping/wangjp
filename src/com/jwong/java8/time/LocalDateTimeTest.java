aefb5773da181a9bcad332b15e0c48b21025f3e6
jwong <wang_jiping@hithinksoft.com>
2017-12-27 10:26:23 +0800

    about bugfix

---
+++ src/com/jwong/java8/time/LocalDateTimeTest.java
@@ -0,0 +1,18 @@
+package com.jwong.java8.time;
+
+import org.junit.Test;
+
+/**
+ * LocalDateTimeTest class
+ *
+ * @author J.Wong
+ * @date 2017/12/27
+ */
+public class LocalDateTimeTest {
+
+    @Test
+    public void test1() {
+
+    }
+
+}
