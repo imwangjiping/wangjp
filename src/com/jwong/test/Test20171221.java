44f7bafeef74a3552307a1a1507e8095a4264ef1
wang_jiping <wang_jiping@dhc.com.cn>
2018-11-14 16:18:51 +0800

    添加部分test

--- src/com/jwong/test/Test20171221.java
+++ src/com/jwong/test/Test20171221.java
@@ -1,6 +1,12 @@
 package com.jwong.test;
 
+import org.junit.Test;
+
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Test20171221 class
@@ -20,6 +26,51 @@ public class Test20171221 {
 
         System.out.println(lastMonthStart);
         System.out.println(lastMonthEnd);
+
+    }
+
+
+    @Test
+    public void test2() {
+        String originalStr = "(R1) 可乐 (Q15 - Q15.您对以下饮料的喜欢程度是什么？请用1-5分打分，1分表示一点也不喜欢；5分表示极其喜欢。 【横向单选，1个others在纵向】 )";
+        String needToRemoveEndStr = "Q15 - Q15.您对以下饮料的喜欢程度是什么？请用1-5分打分，1分表示一点也不喜欢；5分表示极其喜欢。 【横向单选，1个others在纵向】";
+        if (originalStr.contains(needToRemoveEndStr)) {
+            System.out.println(originalStr.substring(0, originalStr.indexOf(needToRemoveEndStr) - 1).trim());
+            ;
+        }
+    }
+
+    @Test
+    public void test3() {
+        List<String> list = new ArrayList() {{
+
+            add("aa");
+            add("a");
+            add("bb");
+
+        }};
+        System.out.println(list.contains("b"));
+
+        System.out.println(list.indexOf("bb"));
+
+    }
+
+    @Test
+    public void test4() {
+        Map<String, String> map = new HashMap<String, String>() {{
+            put("a", "aa");
+            put("b", "bb");
+        }};
+
+        map.forEach((key, value) -> System.out.println(key + "-" + value));
+
+    }
+
+    @Test
+    public void test5(){
+        String str = "Q32. 拥有的社交媒体账号 - ^questID('Q32. ')^请问您拥有哪些社交媒体账号？ ^SAMA('[复选]')^";
+
+        System.out.println(str.replaceAll("\\^.*?\\^", "***"));
 
     }
 
--- src/com/jwong/test/TestLocalDateTime.java
+++ src/com/jwong/test/TestLocalDateTime.java
@@ -3,6 +3,8 @@ package com.jwong.test;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * TestLocalDateTime class
@@ -22,5 +24,31 @@ public class TestLocalDateTime {
         localDateTime = local.plusMonths(1L).minusDays(1L);
         localDateTime = local.plusYears(1L).minusDays(1);
         System.out.println(LocalDateTime.of(localDateTime.getYear(), localDateTime.getMonthValue(), localDateTime.getDayOfMonth(), 23, 59, 59, 999999999));
+    }
+
+
+    @Test
+    public void test2() {
+        List<String> list = new ArrayList() {{
+            add("1");
+            add("2");
+            add("3");
+            add("4");
+            add("5");
+        }};
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if ("2".equals(list.get(i))) {
+                for (int i1 = list.size() - 1; i1 >= 0; i1--) {
+                    if ("1".equals(list.get(i1))) {
+                        list.remove(i);
+                        list.remove(i1);
+                    }
+                }
+            }
+        }
+
+        System.out.println(list.toString());
+
     }
 }
